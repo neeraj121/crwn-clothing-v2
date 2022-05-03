33ba031c4f3ba091f12532c4b88aaf92a4804215
neeraj-grow <neeraj@grow.ae>
2022-05-03 18:30:53 +0400

    CartDropdown changed to styled component

--- src/components/CartDropdown/CartDropdown.tsx
+++ src/components/CartDropdown/CartDropdown.tsx
@@ -1,9 +1,9 @@
 import React, { useContext } from "react";
 import { useNavigate } from "react-router-dom";
 import { CartContext } from "../../contexts/Cart.context";
-import CartItem from "../CartItem/CartItem";
 import Button from "../Button/Button";
-import "./CartDropdown.scss";
+import CartItem from "../CartItem/CartItem";
+import { CartDropdownContainer, CartItems, EmptyMessage } from "./CartDropdown.styles";
 
 interface CartDropdownProps {}
 
@@ -16,14 +16,16 @@ const CartDropdown: React.FC<CartDropdownProps> = () => {
     };
 
     return (
-        <div className="cart-dropdown-container">
-            <div className="cart-items">
-                {cartItems.map((item) => (
-                    <CartItem key={item.id} cartItem={item} />
-                ))}
-            </div>
+        <CartDropdownContainer>
+            <CartItems>
+                {
+                    cartItems.length ? cartItems.map((item) => (
+                        <CartItem key={item.id} cartItem={item} />
+                    )) : <EmptyMessage>Your cart is empty</EmptyMessage>
+                }
+            </CartItems>
             <Button onClick={goToCheckoutHandler}>GO TO CHECKOUT</Button>
-        </div>
+        </CartDropdownContainer>
     );
 };
 
