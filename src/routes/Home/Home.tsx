810705e14f95c1278071fe7c621018cfd602a2c0
neeraj-grow <neeraj@grow.ae>
2022-05-04 15:37:49 +0400

    Build Trigger attempt

--- src/routes/Home/Home.tsx
+++ src/routes/Home/Home.tsx
@@ -1,10 +1,9 @@
 import React from "react";
-import Directory from "../../components/Directory/Directory";
 
 interface HomeProps {}
 
 const Home: React.FC<HomeProps> = () => {
-    return <Directory />;
+    return <div />;
 };
 
 export default Home;
