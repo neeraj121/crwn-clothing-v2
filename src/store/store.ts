5e9199eee09f4b8c02d743c87da05614b8f2bc1e
neeraj-grow <neeraj@grow.ae>
2022-05-10 19:29:28 +0400

    Redux Persist + Redux Dev Tools

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,11 +1,28 @@
-import { createStore, applyMiddleware, compose } from "redux";
+import { createStore, applyMiddleware } from "redux";
 import logger from "redux-logger";
 import { rootReducer } from "./root-reducer";
 
-const middlewares = [logger];
-const composedEnhancers = compose(applyMiddleware(...middlewares));
+import { composeWithDevTools } from "redux-devtools-extension/developmentOnly";
 
-export const store = createStore(rootReducer, composedEnhancers);
+import storage from "redux-persist/lib/storage";
+import persistReducer from "redux-persist/es/persistReducer";
+import persistStore from "redux-persist/es/persistStore";
+
+const persistConfig = {
+    key: "root",
+    storage,
+    blacklist: ["user"],
+};
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+const middlewares = [];
+process.env.NODE_ENV === "development" && middlewares.push(logger);
+
+const composedEnhancers = composeWithDevTools(applyMiddleware(...middlewares));
+
+export const store = createStore(persistedReducer, composedEnhancers);
+
+export const persistor = persistStore(store);
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
