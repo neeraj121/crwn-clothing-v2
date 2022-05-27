06c6d2e56a856801b138631a76d36ca728b6c8bb
neeraj-grow <neeraj@grow.ae>
2022-05-27 20:23:08 +0400

    Category saga typed

--- src/store/categories/categories.saga.ts
+++ src/store/categories/categories.saga.ts
@@ -1,27 +1,27 @@
-import { all, call, put, takeLatest } from "redux-saga/effects";
+import { all, call, put, takeLatest } from "typed-redux-saga";
 import { getCategoriesAndDocuments } from "../../utils/firebase/firebase.utils";
 import {
     fetchCategoriesError,
     fetchCategoriesSuccess,
 } from "./categories.action";
-import { CATEGORIES_ACTION_TYPES, FirestoreCategory } from "./categories.types";
+import { CATEGORIES_ACTION_TYPES } from "./categories.types";
 
-export function* fetchCategoriesAsync(): Generator {
+export function* fetchCategoriesAsync() {
     try {
-        const categories = yield call(getCategoriesAndDocuments);
-        yield put(fetchCategoriesSuccess(categories as FirestoreCategory[]));
+        const categories = yield* call(getCategoriesAndDocuments);
+        yield* put(fetchCategoriesSuccess(categories));
     } catch (error) {
-        yield put(fetchCategoriesError(error as Error));
+        yield* put(fetchCategoriesError(error as Error));
     }
 }
 
 export function* onFetchCategories() {
-    yield takeLatest(
+    yield* takeLatest(
         CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_START,
         fetchCategoriesAsync
     );
 }
 
 export function* categoriesSagas() {
-    yield all([call(onFetchCategories)]);
+    yield* all([call(onFetchCategories)]);
 }
