51642bc6ec1c013eed289c42dfd6bd01a0b3f686
neeraj-grow <neeraj@grow.ae>
2022-06-09 20:51:44 +0400

    Function return value memoization with useMemo

--- src/components/CartDropdown/CartDropdown.tsx
+++ src/components/CartDropdown/CartDropdown.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback } from "react";
+import React, { useCallback, useMemo } from "react";
 import { useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import { selectCartItems } from "../../store/cart/cart.selector";
@@ -12,13 +12,30 @@ import {
 
 interface CartDropdownProps {}
 
+const sleep = (milliseconds: number): void => {
+    var start = new Date().getTime();
+    for (var i = 0; i < 1e7; i++) {
+        if (new Date().getTime() - start > milliseconds) {
+            break;
+        }
+    }
+};
+
 const CartDropdown: React.FC<CartDropdownProps> = () => {
     const cartItems = useSelector(selectCartItems);
     const navigate = useNavigate();
+    const [count, setCount] = useState(0);
 
     const goToCheckoutHandler = useCallback(() => {
         navigate("/checkout");
     }, []);
+
+    const hundredCount = useMemo(() => {
+        console.log("start");
+        sleep(2000);
+        console.log("end");
+        return count + 100;
+    }, [count]);
 
     return (
         <CartDropdownContainer>
