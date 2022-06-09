a91ff1ccc2d732a7cc90a9ca5456bd14996f9b2d
neeraj-grow <neeraj@grow.ae>
2022-06-09 21:40:32 +0400

    Code Splitting, Dynamic Imports, Lazy and Suspense

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,35 @@
-import React, { useEffect } from "react";
+import React, { lazy, Suspense, useEffect } from "react";
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
-import Home from "./routes/Home/Home";
 import Navigation from "./routes/Navigation/Navigation";
-import Authentication from "./routes/Authentication/Authentication";
-import Shop from "./routes/Shop/Shop";
-import Checkout from "./routes/Checkout/Checkout";
 import { checkUserSession } from "./store/user/user.action";
 import { useDispatch } from "react-redux";
+import Spinner from "./components/Spinner/Spinner";
 
-interface AppProps {}
+const Home = lazy(() => import("./routes/Home/Home"));
+const Shop = lazy(() => import("./routes/Shop/Shop"));
+const Checkout = lazy(() => import("./routes/Checkout/Checkout"));
+const Authentication = lazy(
+    () => import("./routes/Authentication/Authentication")
+);
 
-export const App: React.FC<AppProps> = () => {
+export const App: React.FC = () => {
     const dispatch = useDispatch();
     useEffect(() => {
         dispatch(checkUserSession());
     }, [dispatch]);
 
     return (
-        <Routes>
-            <Route path="/" element={<Navigation />}>
-                <Route index element={<Home />} />
-                <Route path="shop/*" element={<Shop />} />
-                <Route path="checkout" element={<Checkout />} />
-                <Route path="auth" element={<Authentication />} />
-            </Route>
-        </Routes>
+        <Suspense fallback={<Spinner />}>
+            <Routes>
+                <Route path="/" element={<Navigation />}>
+                    <Route index element={<Home />} />
+                    <Route path="shop/*" element={<Shop />} />
+                    <Route path="checkout" element={<Checkout />} />
+                    <Route path="auth" element={<Authentication />} />
+                </Route>
+            </Routes>
+        </Suspense>
     );
 };
 
